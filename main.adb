170b7cdcc8c6a7858ade80fe278b0ab70d05d87b
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-23 08:53:37 +0200

    Add main.adb as entry point for the program

---
+++ main.adb
@@ -0,0 +1,9 @@
+-- main.adb
+-- Entry point for generic runs.
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+begin
+   Put_Line ("Rice Coding & Golomb Coding Implementation.");
+   Put_Line ("Please run the test suite to verify code validity: ./bin/tests");
+end Main;
